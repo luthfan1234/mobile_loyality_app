494e9b183b96e75959f79f6b341c54a823d31126
_umannn <134380467+luthfan1234@users.noreply.github.com>
2025-09-30 14:35:52 +0700

    Refactor PointScreen to StatefulWidget and enhance UI with animations and point redemption features

--- lib/screen/point_screen.dart
+++ lib/screen/point_screen.dart
@@ -1,10 +1,183 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/screen/membership_screen.dart';
+import 'package:flutter_application_2/screen/point_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import '../utils/page_route_animation.dart';
 
-class PointScreen extends StatelessWidget {
+class PointScreen extends StatefulWidget {
   const PointScreen({super.key});
 
   @override
+  State<PointScreen> createState() => _PointScreenState();
+}
+
+class _PointScreenState extends State<PointScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/home.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child:
+            Column(
+                  children: [
+                    const SizedBox(height: 24),
+                    Expanded(
+                      child: Stack(children: [_Scrollable_Contain(context)]),
+                    ),
+                  ],
+                )
+                .animate()
+                .fadeIn(duration: 800.ms, delay: 300.ms)
+                .slideY(begin: 0.1, end: 0, curve: Curves.easeOut),
+      ),
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Widget _Scrollable_Contain(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(top: 50),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+      ),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 22),
+                Row(
+                  children: [
+                    Text(
+                      'Redeem Your Points',
+                      style: GoogleFonts.inter(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black,
+                        height: 1.5,
+                      ),
+                    ),
+                    const Spacer(),
+                    Text(
+                      'See All',
+                      style: GoogleFonts.inter(
+                        fontSize: 13,
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0xFF7743DB),
+                        height: 1.5,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                SizedBox(
+                  height: 204,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 3,
+                    itemBuilder: (context, index) => Container(
+                      width: 132,
+                      height: 196,
+                      margin: EdgeInsets.only(
+                        left: index == 0 ? 20 : 0,
+                        right: 16,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            // ignore: deprecated_member_use
+                            color: Colors.black.withOpacity(0.15),
+                            blurRadius: 2,
+                            spreadRadius: 0,
+                            offset: const Offset(1, 0),
+                          ),
+                        ],
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(12),
+                                topRight: Radius.circular(12),
+                              ),
+                              child: Image.asset(
+                                'assets/images/discount${index + 1}.png',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top: 110,
+                            left: 12,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  index == 0
+                                      ? '10% discount \nvoucher'
+                                      : index == 1
+                                      ? '25% discount \nvoucher'
+                                      : '30% discount \nvoucher',
+                                  style: GoogleFonts.inter(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black,
+                                    height: 1.5,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  index == 0
+                                      ? '1000 points'
+                                      : index == 1
+                                      ? '2500 points'
+                                      : '3000 points',
+                                  style: GoogleFonts.inter(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600,
+                                    color: const Color(0xFFFCB351),
+                                    height: 1.5,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ).animate().fadeIn(delay: (300 * (index + 1)).ms).slideX(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
