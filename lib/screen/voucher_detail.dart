8b9256646bf85fb55af79096817dec5dc2e21575
_umannn <134380467+luthfan1234@users.noreply.github.com>
2025-10-02 10:57:42 +0700

    Remove unused import for flutter_animate in VoucherDetailScreen

--- lib/screen/voucher_detail.dart
+++ lib/screen/voucher_detail.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 
 class VoucherDetailScreen extends StatefulWidget {
   const VoucherDetailScreen({super.key});
